b9db338c289552a5f1bdf5631b99852108ef789f
Thomas Jowett <thomas_jowett@hotmail.co.uk>
2024-09-25 16:34:30 +0100

    save images to the Pictures folder with datetime filename

--- python/picamera.py
+++ python/picamera.py
@@ -1,14 +1,23 @@
 import sys
+import os
 import tkinter as tk
 from tkinter import messagebox
 from picamera2 import Picamera2, Preview
+from datetime import datetime
+
+pictures_folder = os.path.join(os.path.expanduser("~"), "Pictures")
+
+os.makedirs(pictures_folder, exist_ok=True)
 
 class CameraApp(tk.Tk):
     def __init__(self):
         super().__init__()
         
         self.picam2 = Picamera2()
-        self.picam2.start_preview(Preview.QTGL)
+        preview_config = self.picam2.create_preview_configuration(main={"size":(640, 480)})
+        self.capture_config = self.picam2.create_still_configuration()
+        self.picam2.configure(preview_config)
+        self.picam2.start_preview(Preview.QTGL, x=0, y=0)
         self.picam2.start()
         
         self.title("Camera App")
@@ -20,15 +29,26 @@ class CameraApp(tk.Tk):
         
     def take_picture(self):
         try:
-            image = self.picam2.capture_file('./image.jpg')
-            messagebox.showinfo("Success", "Image captured successfully.")
+            current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+            filename = f"{current_time}.jpg"
+            filepath = os.path.join(pictures_folder, filename)
+            image = self.picam2.switch_mode_and_capture_image(self.capture_config)
+            print(f"Image saved as: {filepath}")
+            image.save(filepath)
         except Exception as e:
             messagebox.showerror("Error", f"Failed to capture image:\n{e}")
             
     def exit_fullscreen(self, event=None):
         self.attributes('-fullscreen', False)
         self.quit()
+        
+    def close(self):
+        self.picam2.close()
+        self.master.destroy()
     
 if __name__ == '__main__':
     app = CameraApp()
-    app.mainloop()
+    try:
+        app.mainloop()
+    except KeyboardInterupt:
+        app.close()
