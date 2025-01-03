f43555d6d669d4bdbd778e5915a78d763725819e
Thomas Jowett <thomas_jowett@hotmail.co.uk>
2025-01-03 15:13:43 +0000

    removing focus function

--- python/QTPicamera.py
+++ python/QTPicamera.py
@@ -5,7 +5,6 @@ from picamera2.previews.qt import QGlPicamera2
 from datetime import datetime, timedelta
 import os
 import time
-import cv2
 
 pictures_folder = os.path.join(os.path.expanduser("~"), "Pictures")
 os.makedirs(pictures_folder, exist_ok=True)
@@ -37,26 +36,6 @@ class CaptureThread(QtCore.QThread):
         except Exception as e:
             self.image_captured.emit(f"Error: {e}")
 
-class ImageProcess(QtCore.QThread):
-    metadata_captured = QtCore.pyqtSignal(float)
-    
-    def __init__(self, picam2, parent=None):
-        super().__init__(parent)
-        self.picam2 = picam2
-        
-    def run(self):
-        try:
-            im = self.picam2.capture_array()
-            gray_image = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-            laplacian = cv2.Laplacian(gray_image, cv2.CV_64F)
-            variance = laplacian.var()
-            focus_value = variance
-            print(focus_value)
-            self.metadata_captured.emit(focus_value)
-        except Exception as e:
-            self.metadata_captured.emit(0.0)
-
-            
 class CameraApp(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
@@ -85,17 +64,13 @@ class CameraApp(QtWidgets.QMainWindow):
         self.capture_button.clicked.connect(self.take_picture)
         
         # Create a label for displaying the focus
-        self.focus_label = QtWidgets.QLabel("", self)
-        self.focus_label.setAlignment(QtCore.Qt.AlignLeft | QtCore.Qt.AlignVCenter)
+        self.filepath_label = QtWidgets.QLabel("", self)
+        self.filepath_label.setAlignment(QtCore.Qt.AlignLeft | QtCore.Qt.AlignVCenter)
         
-        button_layout.addWidget(self.focus_label)
+        button_layout.addWidget(self.filepath_label)
         button_layout.addWidget(self.capture_button)
         self.statusBar().addPermanentWidget(button_container)  # Add button to status bar
         
-        #self.focus_timer = QtCore.QTimer(self)
-        #self.focus_timer.timeout.connect(self.update_metadata)
-        #self.focus_timer.start(500)
-
         self.show()
         
         self.capture_thread = None
@@ -103,7 +78,7 @@ class CameraApp(QtWidgets.QMainWindow):
 
     def take_picture(self):
         self.capture_button.setEnabled(False)
-        self.focus_label.setText("")
+        self.filepath_label.setText("")
         self.capture_thread = CaptureThread(self.picam2)
         self.capture_thread.image_captured.connect(self.on_image_captured)
         self.capture_thread.start()
@@ -113,19 +88,7 @@ class CameraApp(QtWidgets.QMainWindow):
         if filepath.startswith("Error:"):
            QtWidgets.QMessageBox.critical(self, "Error", filepath)
         else:
-           self.focus_label.setText(f"{filepath}")
-           
-    def on_metadata_captured(self, focus_value):
-        self.focus_label.setText(f"Focus: {focus_value}")
-    
-    def update_metadata(self):
-        try:
-            self.image_process_thread = ImageProcess(self.picam2)
-            self.image_process_thread.metadata_captured.connect(self.on_metadata_captured)
-            self.image_process_thread.start()
-        except Exception as e:
-            print(f"Error updating focus: {e}")
-            self.focus_label.setText("Focus: Error")
+           self.filepath_label.setText(f"{filepath}")
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
